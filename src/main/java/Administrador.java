2ad0cf2872d41fffead17ed4cf89910c8d941f6f
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-13 11:51:11 -0300

    Agrego la funcionalidad de procesar comandos del administrador

--- src/main/java/Administrador.java
+++ src/main/java/Administrador.java
@@ -1,11 +1,38 @@
 public class Administrador {
     private Banco banco;
+    private ServicioComando servicioComando;
 
-    public void crearCuenta(TipoCuenta tipoCuenta, String usuario, String direccion) {
+    private TipoCuenta obtenerTipoCuenta(String argumento) {
+        if (argumento.equals("corriente")) {
+            return TipoCuenta.CuentaCorriente;
+        }
+        return null;
+    }
+
+    private void crearCuenta(TipoCuenta tipoCuenta, String usuario, String direccion) {
         this.banco.crearCuenta(tipoCuenta, usuario, direccion);
     }
 
-    public void eliminarCuenta(String usuario) {
+    private void eliminarCuenta(String usuario) {
         this.banco.eliminarCuenta(usuario);
+    }
+
+    public Administrador(Banco banco, ServicioComando servicioComando) {
+        this.banco = banco;
+        this.servicioComando = servicioComando;
+    }
+
+    public void procesarComando() {
+        var comando = this.servicioComando.obtenerComando();
+        var argumentos = comando.split(" ");
+        if (argumentos[0].equals("crear")) {
+            this.crearCuenta(
+                    this.obtenerTipoCuenta(argumentos[1]),
+                    argumentos[2],
+                    argumentos[3]
+            );
+        } else if (argumentos[0].equals("eliminar")) {
+            this.eliminarCuenta(argumentos[1]);
+        }
     }
 }
