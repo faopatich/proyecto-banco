635eb1d98998cda0760ac1082a0ad8f500236870
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-13 11:13:48 -0300

    Agrego el saldo a la cuenta

--- src/main/java/Cuenta.java
+++ src/main/java/Cuenta.java
@@ -2,10 +2,19 @@ public class Cuenta {
     private TipoCuenta tipoCuenta;
     private String nombrePropietario;
     private String direccion;
+    private int saldo;
 
     Cuenta(TipoCuenta tipoCuenta, String nombrePropietario, String direccion) {
         this.tipoCuenta = tipoCuenta;
         this.nombrePropietario = nombrePropietario;
         this.direccion = direccion;
+        this.saldo = 0;
+    }
+
+    public String obtenerNombrePropietario() {
+        return this.nombrePropietario;
+    }
+    public int obtenerSaldo() {
+        return this.saldo;
     }
 }
