4999d2f0dc867a403291d40e54470419c257df79
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-14 13:12:52 -0300

    Implemento el método manejarDeposito en GestorTransacciones

--- src/main/java/proyectoBanco/usuarios/GestorTransacciones.java
+++ src/main/java/proyectoBanco/usuarios/GestorTransacciones.java
@@ -1,14 +1,25 @@
 package proyectoBanco.usuarios;
 
 import proyectoBanco.banco.ServicioBanco;
+import proyectoBanco.cuentas.Cuenta;
+
+import java.util.HashMap;
 
 public class GestorTransacciones extends Usuario {
+    private HashMap<CredencialesUsuario, Cuenta> cuentas;
+
     public GestorTransacciones(ServicioBanco servicioBanco, CredencialesUsuario credencialesUsuario) {
         super(servicioBanco, credencialesUsuario);
+        this.cuentas = new HashMap<>();
     }
 
     public boolean manejarDeposito(CredencialesUsuario credencialesUsuario, int saldo) {
-        return false;
+        var cuenta = this.cuentas.get(credencialesUsuario);
+        if (cuenta == null) {
+            return false;
+        }
+        cuenta.depositar(saldo);
+        return true;
     }
     public boolean manejarRetiro(CredencialesUsuario credencialesUsuario, int saldo) {
         return false;
