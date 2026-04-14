749c4e1e023d8c299fe9f8ff794de0bedb885b7c
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-14 16:56:09 -0300

    Implemento un main para probar el código

--- src/main/java/proyectoBanco/Main.java
+++ src/main/java/proyectoBanco/Main.java
@@ -1,49 +1,53 @@
 package proyectoBanco;
 
 import proyectoBanco.administrador.Administrador;
-import proyectoBanco.banco.Sucursal;
+import proyectoBanco.banco.*;
 import proyectoBanco.comandos.ServicioComando;
 import proyectoBanco.cuentas.CreadorCuenta;
+import proyectoBanco.cuentas.Cuenta;
 import proyectoBanco.cuentas.TipoCuenta;
-import proyectoBanco.banco.ServicioBanco;
+import proyectoBanco.usuarios.Cliente;
 import proyectoBanco.usuarios.PerfilUsuario;
-import proyectoBanco.usuarios.Usuario;
+import proyectoBanco.usuarios.RolUsuario;
+import proyectoBanco.usuarios.UsuarioGestorCuentas;
+
+import java.util.HashMap;
+import java.util.HashSet;
 
 class Main {
     public static void main(String[] args) {
-        /*
         var creadorCuenta = new CreadorCuenta();
-        var sucursal = new Sucursal(creadorCuenta);
-        var servicioComando = new ServicioComando();
-        var administrador = new Administrador(sucursal, servicioComando);
-        var servicioBanco = new ServicioBanco(sucursal);
-
-        sucursal.cambiarAdministrador(administrador);
+        var cuentas = new HashMap<String, Cuenta>();
+        var gestorCuentas = new GestorCuentas(cuentas, creadorCuenta);
+        var gestorRoles = new GestorRoles();
+        var gestorUsuarios = new GestorUsuarios(gestorRoles);
+        var gestorTransacciones = new GestorTransacciones(cuentas);
+        var sucursal = new Sucursal(gestorCuentas, gestorTransacciones);
+        var servicioBanco = new ServicioBanco(sucursal, gestorUsuarios);
 
         var perfil1 = new PerfilUsuario("Mateo", "1234seguro", "hoy");
         var perfil2 = new PerfilUsuario("Carlos", "000noseguro", "ayer");
+        var perfilGestor = new PerfilUsuario("Ulises", "gestor444", "mañana");
 
-        var usuario1 = new Usuario(perfil1, servicioBanco);
-        var usuario2 = new Usuario(perfil2, servicioBanco);
+        var usuarioGestorCuentas = new UsuarioGestorCuentas(servicioBanco, perfilGestor.generarCredenciales());
 
-        usuario1.solicitarCrearCuenta(TipoCuenta.CuentaCorriente);
-        usuario2.solicitarCrearCuenta(TipoCuenta.CuentaAhorro);
+        servicioBanco.crearCuenta(perfil1, TipoCuenta.CuentaAhorro);
+        servicioBanco.crearCuenta(perfil2, TipoCuenta.CuentaCorriente);
 
-        administrador.procesarComandos();
+        var rolesGestor = new HashSet<RolUsuario>();
+        rolesGestor.add(RolUsuario.GestorCuentas);
+        servicioBanco.crearUsuario(perfilGestor, rolesGestor);
 
-        usuario1.actualizarCuenta();
-        usuario2.actualizarCuenta();
+        usuarioGestorCuentas.verOperacionesPendientes();
+        usuarioGestorCuentas.resolverOperacion(0);
+        usuarioGestorCuentas.resolverOperacion(0);
 
-        usuario1.verEstadoCuenta();
-        usuario2.verEstadoCuenta();
+        var cliente1 = new Cliente(servicioBanco, perfil1.generarCredenciales());
+        cliente1.actualizarVistaCuenta();
+        cliente1.verEstadoCuenta();
 
-        usuario1.depositar(100);
-
-        usuario1.transferir("Carlos", 200);
-
-        System.out.println("\n\nLuego de la transferencia\n");
-        usuario1.verEstadoCuenta();
-        usuario2.verEstadoCuenta();
-         */
+        var cliente2 = new Cliente(servicioBanco, perfil2.generarCredenciales());
+        cliente2.actualizarVistaCuenta();
+        cliente2.verEstadoCuenta();
     }
 }
