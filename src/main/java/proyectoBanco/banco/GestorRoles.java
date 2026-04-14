5f2510359ef0757796ab6825fbc38802a7dfdf9e
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-14 13:38:44 -0300

    Agrego el enum RolUsuario

--- src/main/java/proyectoBanco/banco/GestorRoles.java
+++ src/main/java/proyectoBanco/banco/GestorRoles.java
@@ -4,17 +4,24 @@ import proyectoBanco.usuarios.CredencialesUsuario;
 import proyectoBanco.usuarios.RolUsuario;
 
 import java.util.HashMap;
-import java.util.List;
+import java.util.Set;
 
 public class GestorRoles {
-    private final HashMap<String, List<RolUsuario>> rolesUsuario;
+    private final HashMap<String, Set<RolUsuario>> rolesUsuario;
 
     public GestorRoles() {
         this.rolesUsuario = new HashMap<>();
     }
 
-    public boolean verificarUsuarioConCredenciales(CredencialesUsuario credencialesUsuario, RolUsuario rolUsuario) {
-        var roles = this.rolesUsuario.get(credencialesUsuario.usuario());
+    public void agregarRolesDeUsuario(String nombre, Set<RolUsuario> rolesUsuario) {
+        if (!this.rolesUsuario.containsKey(nombre)) {
+            this.rolesUsuario.put(nombre, rolesUsuario);
+        } else {
+            this.rolesUsuario.get(nombre).addAll(rolesUsuario);
+        }
+    }
+    public boolean verificarUsuarioConCredenciales(String usuario, RolUsuario rolUsuario) {
+        var roles = this.rolesUsuario.get(usuario);
         if (roles == null || roles.isEmpty()) {
             return false;
         }
---
+++ src/main/java/proyectoBanco/usuarios/RolUsuario.java
@@ -0,0 +1,6 @@
+package proyectoBanco.usuarios;
+
+public enum RolUsuario {
+    Cliente,
+    Admin
+}
