91af3859c1f25f920dd6ee665b49bfd337faaeeb
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-15 09:45:10 -0300

    Actualizo la aplicación del gestor de cuentas para que empiece a pedir datos por consola

--- src/main/java/proyectoBanco/terminal/AplicacionGestorCuentas.java
+++ src/main/java/proyectoBanco/terminal/AplicacionGestorCuentas.java
@@ -1,5 +1,16 @@
 package proyectoBanco.terminal;
 
+import proyectoBanco.banco.*;
+import proyectoBanco.cuentas.CreadorCuenta;
+import proyectoBanco.cuentas.Cuenta;
+import proyectoBanco.usuarios.PerfilUsuario;
+import proyectoBanco.usuarios.RolUsuario;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class AplicacionGestorCuentas {
     private final ServicioComandoGestorCuentas servicioComandoGestorCuentas;
 
@@ -20,7 +31,29 @@ public class AplicacionGestorCuentas {
         while (this.manejarComando());
     }
 
-    public static void main(String[] args) {
+    public static ServicioBanco crearServicioBanco() {
+        var cuentas = new HashMap<String, Cuenta>();
+        var creadorCuenta = new CreadorCuenta();
+        var gestorCuentas = new GestorCuentas(cuentas, creadorCuenta);
+        var gestorTransaccs = new GestorTransacciones(cuentas);
+        var gestorRoles = new GestorRoles();
+        var gestorUsuarios = new GestorUsuarios(gestorRoles);
+        var sucursal = new Sucursal(gestorCuentas, gestorTransaccs);
+        return new ServicioBanco(sucursal, gestorUsuarios);
+    }
 
+    public static void main(String[] args) {
+        var servicioBanco = AplicacionGestorCuentas.crearServicioBanco();
+
+        var perfilGestorCuentas = new PerfilUsuario("Mateo", "235", "La Boca");
+        var roles = new HashSet<>(Set.of(RolUsuario.GestorCuentas));
+        servicioBanco.crearUsuario(perfilGestorCuentas, roles);
+
+        var fabrica = new FabricaComandoGestorCuentas(servicioBanco, perfilGestorCuentas);
+        var servicioEntrada = new ServicioEntrada(new Scanner(System.in));
+        var servicioComandoGestorCuentas = new ServicioComandoGestorCuentas(servicioEntrada, fabrica);
+        var app = new AplicacionGestorCuentas(servicioComandoGestorCuentas);
+
+        app.manejarComandos();
     }
 }
