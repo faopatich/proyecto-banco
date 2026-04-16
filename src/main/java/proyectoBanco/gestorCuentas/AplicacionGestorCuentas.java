e6c32db116434d69601cc9ee552ed236823b758c
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-16 09:04:04 -0300

    Actualizo AplicacionGestorCuentas para usar la sucursal

--- src/main/java/proyectoBanco/gestorCuentas/AplicacionGestorCuentas.java
+++ src/main/java/proyectoBanco/gestorCuentas/AplicacionGestorCuentas.java
@@ -2,7 +2,6 @@ package proyectoBanco.gestorCuentas;
 
 import proyectoBanco.banco.*;
 import proyectoBanco.banco.servicios.*;
-import proyectoBanco.cuentas.CreadorCuenta;
 import proyectoBanco.cuentas.TipoCuenta;
 import proyectoBanco.usuarios.Cliente;
 import proyectoBanco.usuarios.PerfilUsuario;
@@ -16,17 +15,6 @@ import java.util.Set;
 public class AplicacionGestorCuentas {
     public final Scanner scanner;
     public final ServicioEntrada servicioEntrada;
-    public final CreadorCuenta creadorCuenta;
-    public final GestorCuentas gestorCuentas;
-    public final GestorTransacciones gestorTransacciones;
-    public final GestorRoles gestorRoles;
-    public final GestorUsuarios gestorUsuarios;
-    public final ServicioCuentaCliente servicioCuentaCliente;
-    public final ServicioTransaccion servicioTransaccion;
-    public final ServicioGestionCuentas servicioGestionCuentas;
-    public final ServicioCliente servicioCliente;
-    public final ServicioGestorCuentas servicioGestorCuentas;
-    public final ServicioUsuario servicioUsuario;
 
     public ServicioComandoGestorCuentas servicioComandoGestorCuentas;
     public Menu menu;
@@ -34,18 +22,6 @@ public class AplicacionGestorCuentas {
     public AplicacionGestorCuentas() {
         this.scanner = new Scanner(System.in);
         this.servicioEntrada = new ServicioEntrada(scanner);
-        this.creadorCuenta = new CreadorCuenta();
-        this.gestorCuentas = new GestorCuentas(creadorCuenta);
-        this.gestorTransacciones = new GestorTransacciones();
-        this.gestorRoles = new GestorRoles();
-        this.gestorUsuarios = new GestorUsuarios(gestorRoles);
-        this.servicioCuentaCliente = new ServicioCuentaCliente(gestorCuentas);
-        this.servicioTransaccion = new ServicioTransaccion(gestorTransacciones);
-        this.servicioGestionCuentas = new ServicioGestionCuentas(gestorCuentas);
-        this.servicioCliente = new ServicioCliente(gestorUsuarios, servicioCuentaCliente, servicioTransaccion);
-        this.servicioGestorCuentas = new ServicioGestorCuentas(gestorUsuarios, servicioGestionCuentas);
-        this.servicioUsuario = new ServicioUsuario(gestorUsuarios);
-
         this.servicioComandoGestorCuentas = null;
         this.menu = null;
     }
@@ -53,12 +29,14 @@ public class AplicacionGestorCuentas {
     public static void main(String[] args) {
         var aplicacion = new AplicacionGestorCuentas();
 
+        var sucursal1 = new Sucursal();
+
         var perfilGestorCuentas = new PerfilUsuario("Mateo", "123", "24/5/2004");
         var roles = new HashSet<>(Set.of(RolUsuario.GestorCuentas));
         var rolCliente = new HashSet<>(Set.of(RolUsuario.Cliente));
 
         var fabrica = new FabricaComandoGestorCuentas(
-                aplicacion.servicioGestorCuentas,
+                sucursal1.servicioGestorCuentas,
                 perfilGestorCuentas
         );
         aplicacion.servicioComandoGestorCuentas = new ServicioComandoGestorCuentas(
@@ -67,25 +45,30 @@ public class AplicacionGestorCuentas {
         );
         aplicacion.menu = new Menu(aplicacion.servicioComandoGestorCuentas);
 
-        aplicacion.servicioUsuario.crearUsuarioSiNoExiste(perfilGestorCuentas, roles);
+        sucursal1.servicioUsuario.crearUsuarioSiNoExiste(perfilGestorCuentas, roles);
         var usuarioGestorCuentas = new UsuarioGestorCuentas(
                 perfilGestorCuentas,
-                aplicacion.servicioGestorCuentas
+                sucursal1.servicioGestorCuentas
         );
 
         var perfilUsuario1 = new PerfilUsuario("Carlos","hola", "hace mucho");
         var perfilUsuario2 = new PerfilUsuario("Rosa", "lol", "hace relativamente poco");
 
-        aplicacion.servicioUsuario.crearUsuarioSiNoExiste(perfilUsuario1, rolCliente);
-        aplicacion.servicioUsuario.crearUsuarioSiNoExiste(perfilUsuario2, rolCliente);
+        sucursal1.servicioUsuario.crearUsuarioSiNoExiste(perfilUsuario1, rolCliente);
+        sucursal1.servicioUsuario.crearUsuarioSiNoExiste(perfilUsuario2, rolCliente);
 
-        var cliente1 = new Cliente(perfilUsuario1, aplicacion.servicioCliente);
+        var cliente1 = new Cliente(perfilUsuario1, sucursal1.servicioCliente);
+        var cliente2 = new Cliente(perfilUsuario2, sucursal1.servicioCliente);
         cliente1.solicitarCrearCuenta(TipoCuenta.CuentaAhorro);
-        cliente1.solicitarCrearCuenta(TipoCuenta.CuentaCorriente);
+        cliente2.solicitarCrearCuenta(TipoCuenta.CuentaCorriente);
 
         aplicacion.menu.ejecutar();
 
+        cliente1.depositar(200);
+        cliente1.transferir(perfilUsuario2.obtenerNombre(), 400);
+
         cliente1.verEstadoCuenta();
+        cliente2.verEstadoCuenta();
 
         usuarioGestorCuentas.verOperacionesPendientes();
     }
