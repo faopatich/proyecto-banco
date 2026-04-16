118017b63e6879fef85656bae448450432bde8f8
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-16 13:53:35 -0300

    chore: Agrego un BancoMMenu

---
+++ src/main/java/aplicacion/BancoM/menu/BancoMMenu.java
@@ -0,0 +1,34 @@
+package aplicacion.BancoM.menu;
+
+import aplicacion.BancoM.menu.comandos.ComandoMenu;
+import aplicacion.BancoM.menu.comandos.ComandoSalir;
+import aplicacion.interfazComun.Menu;
+
+public abstract class BancoMMenu implements Menu {
+    protected final ServicioComandoMenu servicioComandoMenu;
+
+    BancoMMenu(ServicioComandoMenu servicioComandoMenu) {
+        this.servicioComandoMenu = servicioComandoMenu;
+    }
+
+    protected boolean manejarComando() {
+        ComandoMenu comando;
+        System.out.print("\n> ");
+        comando = this.servicioComandoMenu.obtenerComando();
+        if (comando instanceof ComandoSalir) {
+            return false;
+        }
+        if (comando == null) {
+            System.out.println("Comando desconocido");
+            return true;
+        }
+        comando.ejecutar();
+        return true;
+    }
+
+    protected void manejarComandos() {
+        while (this.manejarComando()) {}
+    }
+
+    public abstract void ejecutar();
+}
